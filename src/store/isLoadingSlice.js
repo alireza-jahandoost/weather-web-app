0a81dbc0c07ff370b58079bffbc5f1a1debe8300
Alireza Jahandoost <alireza.jhd2000@gmail.com>
2022-02-14 11:22:40 +0330

    Create isLoading reducer and actions

--- src/store/isLoadingSlice.js
+++ src/store/isLoadingSlice.js
@@ -5,7 +5,16 @@ const initialState = false;
 const isLoadingSlice = createSlice({
   name: "isLoading",
   initialState,
-  reducers: {},
+  reducers: {
+    loadingBegin: (state) => {
+      return true;
+    },
+    loadingEnd: (state) => {
+      return false;
+    },
+  },
 });
+
+export const { loadingBegin, loadingEnd } = isLoadingSlice.actions;
 
 export default isLoadingSlice.reducer;
