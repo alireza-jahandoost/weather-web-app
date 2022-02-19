a940872803494065920546373f36bb844fd1a1b4
Alireza Jahandoost <alireza.jhd2000@gmail.com>
2022-02-19 08:16:36 +0330

    Remove isLoading slice from store

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import dateReducer from "./dateSlice";
 import locationReducer from "./locationSlice";
 import weatherReducer from "./weatherSlice";
-import isLoadingReducer from "./isLoadingSlice";
 
 const store = configureStore({
   reducer: {
     date: dateReducer,
     weather: weatherReducer,
     location: locationReducer,
-    isLoading: isLoadingReducer,
   },
 });
 
