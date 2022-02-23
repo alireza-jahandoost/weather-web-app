23185ddd636a5b7de25e6648f4d3f733f66cc0ea
Alireza Jahandoost <alireza.jhd2000@gmail.com>
2022-02-23 14:50:31 +0330

    Remove warnings

--- src/components/error-container/error-container.component.jsx
+++ src/components/error-container/error-container.component.jsx
@@ -1,5 +1,3 @@
-import styled from "styled-components";
-
 const ErrorContainer = ({ error }) => {
   return <div>{error}</div>;
 };
--- src/components/location-input/location-input.component.jsx
+++ src/components/location-input/location-input.component.jsx
@@ -1,5 +1,4 @@
 import TextField from "@mui/material/TextField";
-import ErrorContainer from "../error-container/error-container.component";
 
 const LocationInput = ({ errorMessage, onChange, value, disabled }) => {
   if (typeof value !== "string") {
